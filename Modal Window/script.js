6c0410d057d02f756605c0cb5ce11a203e63ebd1
itsthankk <tkingsway3@gmail.com>
2023-03-22 01:58:45 +0100

    add functionalities

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -5,12 +5,28 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+const closeModal = function() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
 
-for(let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].addEventListener("click", function() {
-        console.log("Button Clicked");
-        modal.classList.remove("hidden")
-    }));
-}
+const openModal = function() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
 
+for(let i = 0; i < btnsOpenModal.length ;i++) {
+    (btnsOpenModal[i].addEventListener("click", openModal));
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+ 
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function(e) {
+    if(e.key === "Escape") {
+        if(!modal.classList.contains("hidden")) {
+            closeModal();
+        }
+    }
+})
