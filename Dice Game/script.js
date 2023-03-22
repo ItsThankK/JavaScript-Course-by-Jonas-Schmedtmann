87e38d9d10cee77388cf36dfee8ae24acf44c1d9
itsthankk <tkingsway3@gmail.com>
2023-03-22 20:50:16 +0100

    complete game

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -1,14 +1,14 @@
-'use strict';
- 
-// Selecting elements 
+"use strict";
+
+// Selecting elements
 const player0EL = document.querySelector(".player--0");
 const player1EL = document.querySelector(".player--1");
 
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
-const diceEL =  document.querySelector(".dice");
-const current0EL = document.getElementById("current--0")
-const current1EL = document.getElementById("current--1")
+const diceEL = document.querySelector(".dice");
+const current0EL = document.getElementById("current--0");
+const current1EL = document.getElementById("current--1");
 
 const btnRoll = document.querySelector(".btn--roll");
 const btnNew = document.querySelector(".btn--new");
@@ -17,35 +17,99 @@ const btnHold = document.querySelector(".btn--hold");
 // Rolling dice functionality
 score0EL.textContent = 0;
 score1EL.textContent = 0;
-diceEL.classList.add("hidden")
+diceEL.classList.add("hidden");
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores;
+let currentScore;
+let activePlayer;
 
+let playing;
 
-btnRoll.addEventListener("click", function() {
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+
+  playing = true;
+
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+  diceEL.classList.add("hidden");
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove(`player--winner`);
+
+  player0EL.classList.add("player--active");
+  player1EL.classList.remove("player--active");
+};
+
+init();
+
+const switchPlayer = function () {
+  // Switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+
+  player0EL.classList.toggle("player--active");
+  player1EL.classList.toggle("player--active");
+};
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
     // Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    // Display dice 
-    diceEL.classList.remove("hidden")
+    // Display dice
+    diceEL.classList.remove("hidden");
 
-    diceEL.src = `dice-${dice}.png`
+    diceEL.src = `dice-${dice}.png`;
     // Check for roll 1: if true, switch to next player
 
-    if(dice !== 1) {
-        // Add dice to current score
-        currentScore += dice;   
-        //builsing id name dynamically      
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else{
-        // Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-
-        player0EL.classList.toggle("player--active");
-        player1EL.classList.toggle("player--active");
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      //builsing id name dynamically
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
     }
-})
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //Add current score to active player's score
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //Check score is >= 100
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEL.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      //Switch the player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", function () {
+  //Restarting the game
+  init();
+});
